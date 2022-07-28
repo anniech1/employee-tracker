eac7ec94ee977715873f8559062f9e2081d8870d
Annie Hosch <anniehosch@Annies-Air.lan>
2022-07-28 02:54:03 -0400

    added constraint for forgein key

--- db/schema.sql
+++ db/schema.sql
@@ -11,18 +11,22 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    FOREIGN KEY(department_id) 
-    REFERENCES department(id)
-    ON DELETE CASCADE
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    CONSTRAINT fk_departments
+        FOREIGN KEY(department_id) 
+        REFERENCES department(id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY(role_id) 
-    REFERENCES role(id)
-    ON DELETE CASCADE,
-    manager_id INT
+    manager_id INT,
+    role_id INT,
+    CONSTRAINT fk_role
+        FOREIGN KEY(role_id) 
+        REFERENCES role(id)
+        ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,20 +4,25 @@ INSERT INTO department(name)VALUES
 ("Administration"),
 ("Marketing"),
 ("Human Resources"),
-("Sales"),
 ("Finance");
 
-INSERT INTO role(title,salary)VALUES
-("Manager", 100000),
-("Employee", 75000),
-("Intern" 50000);
+INSERT INTO role(title,salary, department_id)VALUES
+("Project Manager", 100000, 1),
+("Advertisment Editor", 75000, 2),
+("Copy Editor", 50000, 2),
+("HR Coordinator", 75000, 3),
+("Hiring Assistant", 60000, 3),
+("Finanical Analyst", 55000, 4),
+("Finanical Intern", 50000, 4),
+("Payroll Clerk", 45000, 4),
+("Chief Finacial Officer", 100000, 4);
 
-INSERT INTO employee(first_name, last_name)VALUES
-("John", "Miller"),
-("Beatrice", "Lee"),
-("Selena", "Garcia"),
-("Ophelia", "Spear"),
-("Wendy", "Rove"),
-("Kevin", "Nguyen"),
-("Emily", "Fisher"),
-("Fred", "Cook");
+INSERT INTO employee(first_name, last_name, role_id, manager_id)VALUES
+("John", "Miller", 1, null),
+("Beatrice", "Lee", 2, null),
+("Selena", "Garcia", 3, 2),
+("Ophelia", "Spear", 3, 3),
+("Wendy", "Rove", 4, null),
+("Kevin", "Nguyen", 4, 5),
+("Emily", "Fisher", 4, 5),
+("Fred", "Cook", 4, null);
